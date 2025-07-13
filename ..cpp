2f2e1999efc5cc3e21f8dad72e2651b4e6a482e8
Rahul Yadav <captainrahul2005@gmail.com>
2025-07-13 17:26:04 +0530

    new files added

--- ..cpp
+++ ..cpp
@@ -4,5 +4,10 @@ int main(){
     int n ; 
     cin >> n;
     cout << "The number is: " << n << endl;
+    if (n % 2 == 0) {
+        cout << "The number is even." << endl;
+    } else {
+        cout << "The number is odd." << endl;
+    }
+    cout <<endl ;
     return 0;
-}
